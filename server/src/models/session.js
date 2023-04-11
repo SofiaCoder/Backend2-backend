2f2f3c9b7db89772a08058bacc34c3adc82b46c4
sofiaCoder <sofia.laas@gmail.com>
2023-04-11 17:26:15 +0200

    Changes made in wrong branch

--- server/src/models/session.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const sessionSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  token: { type: String, required: true },
-});
-
-module.exports = mongoose.model("Session", sessionSchema);
