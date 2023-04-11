ae4ab299091ca059774d5d334ba2b55a07006cfa
sofiaCoder <sofia.laas@gmail.com>
2023-04-11 17:03:52 +0200

    Test created for login and register

---
+++ server/src/index.test.js
@@ -0,0 +1,28 @@
+const request = require('supertest');
+const { server } = require('./server');
+
+describe("Testing authRouter endpoints", () => {
+    test('POST /register should return 201 if req.body is good', async () => {
+        const respons = await request(server).post('/register').send({username: 'Soffi', password: 'hemligt'});
+
+        expect(respons.status).toBe(201);
+    })
+
+    test('POST /register should return 400 if req.body is unfulfilled', async () => {
+        const respons = await request(server).post('/register').send({});
+
+        expect(respons.status).toBe(400);
+    })
+
+    test('POST /login should return 200 if req.body is good', async () => {
+        const respons = await request(server).post('/login').send({username: 'Skantz', password: 'hemligt'});
+
+        expect(respons.status).toBe(200);
+    })
+
+    test('POST /login should return 400 if req.body is unfulfilled', async () => {
+        const respons = await request(server).post('/login').send({});
+
+        expect(respons.status).toBe(400);
+    })
+})
