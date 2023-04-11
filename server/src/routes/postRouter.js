1f81e4347a91f4ab660c99d5b3e0515dfc1ac5dc
sofiaCoder <sofia.laas@gmail.com>
2023-04-11 17:03:29 +0200

    postRouter created

---
+++ server/src/routes/postRouter.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const postRouter = express.Router()
+
+postRouter.get('/')
+postRouter.post('/')
+postRouter.patch('/')
+postRouter.delete('/')
+
+exports.postRouter = postRouter
