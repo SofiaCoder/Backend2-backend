3c32b486fdd18aec67cf94108f49ba92b2995917
sofiaCoder <sofia.laas@gmail.com>
2023-04-14 23:15:05 +0200

    try catch added

--- server/src/controllers/posts/deletePosts.js
+++ server/src/controllers/posts/deletePosts.js
@@ -3,23 +3,27 @@ const main = require('../../databas')
 const { ObjectId } = require('mongodb')
 
 exports.deletePosts = async function deletePosts(req, res) {
-    const { postsCollection } = await main();
-    const schema = joi.object({
-        postID: joi.string()
-    })
+    try {
+        const { postsCollection } = await main();
+        const schema = joi.object({
+            postID: joi.string()
+        })
 
-    const { value, error } = schema.validate(req.body);
-    if (error) {
-        return res.status(400).send(error.details[0].message)
-    }
-    const { postID } = value
-    const postIDtoDelete = new ObjectId(postID)
+        const { value, error } = schema.validate(req.body);
+        if (error) {
+            return res.status(400).send(error.details[0].message)
+        }
+        const { postID } = value
+        const postIDtoDelete = new ObjectId(postID)
 
-    const posts = await postsCollection.deleteOne({"_id": postIDtoDelete})
-    
-    if (posts.deletedCount === 1) {
-        res.status(200).send(`Post deleted successfully`)
-    } else {
-        res.status(500).send(`Query didnt match any document. 0 document deleted.`)
+        const posts = await postsCollection.deleteOne({"_id": postIDtoDelete})
+        
+        if (posts.deletedCount === 1) {
+            res.status(200).send(`Post deleted successfully`)
+        } else {
+            res.status(500).send(`Query didnt match any document. 0 document deleted.`)
+        }
+    } catch (error) {
+        res.status(500).send(`Internal server error - ${error}`)
     }
 }
