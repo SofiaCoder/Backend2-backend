faa3d1993f231b2ff28cf34ce5eb98c833d8409c
sofiaCoder <sofia.laas@gmail.com>
2023-04-14 22:25:19 +0200

    deletePost created

--- server/src/controllers/posts/deletePosts.js
+++ server/src/controllers/posts/deletePosts.js
@@ -1,3 +1,25 @@
-exports.deletePosts = function deletePosts(req, res) {
-    res.send('deletePosts reached')
+const joi = require('joi')
+const main = require('../../databas')
+const { ObjectId } = require('mongodb')
+
+exports.deletePosts = async function deletePosts(req, res) {
+    const { postsCollection } = await main();
+    const schema = joi.object({
+        postID: joi.string()
+    })
+
+    const { value, error } = schema.validate(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message)
+    }
+    const { postID } = value
+    const postIDtoDelete = new ObjectId(postID)
+
+    const posts = await postsCollection.deleteOne({"_id": postIDtoDelete})
+    
+    if (posts.deletedCount === 1) {
+        res.status(200).send(`Post deleted successfully`)
+    } else {
+        res.status(500).send(`Query didnt match any document. 0 document deleted.`)
+    }
 }
