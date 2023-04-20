e5018691dc4789075c1bd40644f1dbd46fcc1f20
sofiaCoder <sofia.laas@gmail.com>
2023-04-20 19:10:57 +0200

    Validation added

--- server/src/controllers/posts/getPosts.js
+++ server/src/controllers/posts/getPosts.js
@@ -7,8 +7,11 @@ exports.getPosts = async function getPosts(req, res) {
         const { postsCollection } = await main();
 
         const posts = await postsCollection.find({user_id: userID}).toArray()
-        
-        res.status(200).send(posts)
+        if(posts.length === 0) {
+            res.status(404).send('No posts found in database')
+        } else {
+            res.status(200).json(posts)
+        }
         
     } catch (error) {
         res.status(500).send(`Internal server error - ${error}`)
