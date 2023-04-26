d924bb457d853c863acc380c9f17a7363fd9911c
sofiaCoder <sofia.laas@gmail.com>
2023-04-26 12:03:25 +0200

    cookie-parser added and friendRouter path created

--- server/src/server.js
+++ server/src/server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const server = express();
 const { authRouter } = require("./routes/authRouter");
 const { postRouter } = require("./routes/postRouter");
-server.use(express.json());
+const { friendRouter } = require("./routes/friendRouter");
+const cookieParser = require('cookie-parser')
 const cors = require("cors");
+server.use(express.json());
+server.use(cookieParser());
 server.use(
   cors({
     origin: "http://127.0.0.1:3000",
@@ -11,8 +14,8 @@ server.use(
   })
 );
 
-
 server.use("/auth", authRouter);
 server.use("/post", postRouter);
+server.use("/friend", friendRouter);
 
 exports.server = server;
