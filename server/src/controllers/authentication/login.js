f2b31e4db48a16699d8571f0b87bd7cc8ce74480
Pskantz <112848766+Pskantz@users.noreply.github.com>
2023-04-26 17:29:06 +0200

    updated so username also is included in post

--- server/src/controllers/authentication/login.js
+++ server/src/controllers/authentication/login.js
@@ -1,18 +1,18 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const main = require("../../databas");
-const joi = require('joi')
+const joi = require("joi");
 
 exports.login = async function login(req, res) {
   try {
     const schema = joi.object({
       username: joi.string().min(1).max(25).required(),
-      password: joi.string().min(4).required()
-    })
+      password: joi.string().min(4).required(),
+    });
 
-    const { value, error } = schema.validate(req.body)
-    if(error) {
-      return res.status(401).send(error.details[0].message)
+    const { value, error } = schema.validate(req.body);
+    if (error) {
+      return res.status(401).send(error.details[0].message);
     }
 
     const { username, password } = value;
@@ -28,17 +28,15 @@ exports.login = async function login(req, res) {
       return res.status(401).json({ message: "Invalid username or password" });
     }
 
-    const token = jwt.sign(
-      { id: user._id }, 
-      process.env.JWT_SECRET, 
-      { expiresIn: "1h" }
-      );
-    res.cookie('authKey', token, {
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.cookie("authKey", token, {
       maxAge: 60 * 60 * 1000,
       httpOnly: true,
-      sameSite: 'none',
-      //secure: true
-    })
+      sameSite: "none",
+      //secure: true,
+    });
     res.status(200).json({ message: "Successfully login" });
   } catch (err) {
     console.error(err);
