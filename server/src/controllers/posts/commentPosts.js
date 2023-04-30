ddbb1c632cde6ad93a8adf2e7889174d60b3f4f5
sofiaCoder <sofia.laas@gmail.com>
2023-04-30 21:14:09 +0200

    Added data to push into comment and edited names

--- server/src/controllers/posts/commentPosts.js
+++ server/src/controllers/posts/commentPosts.js
@@ -5,9 +5,11 @@ const { ObjectId } = require('mongodb');
 exports.commentPosts = async function commentPosts(req, res) {
     try {
         const { postsCollection } = await main()
+        const username = req.username
+        const date = new Date()
 
         const schema = joi.object({
-            comment: joi.string().min(1).max(250).required(),
+            commentBody: joi.string().min(1).max(250).required(),
             postID: joi.string().required()
         })
 
@@ -15,14 +17,20 @@ exports.commentPosts = async function commentPosts(req, res) {
         if (error) {
             return res.status(400).send(error.details[0].message)
         }
-        const { comment, postID } = value;
+        const { commentBody, postID } = value;
         const postIDtoInsert = new ObjectId(postID);
+
+        const comment = {
+            username,
+            date,
+            commentBody
+        }
 
         const respons = await postsCollection.updateOne({_id: postIDtoInsert}, {$push: {comments: comment}})
         if(respons.matchedCount === 0) {
             res.status(404).send('Wrong id, this document was not found')
         } else {
-            res.status(200).send('Comment added')
+            res.status(200).json(comment)
         }
 
     } catch (error) {
